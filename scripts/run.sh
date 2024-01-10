728230d159650edaf06e6d96150c27cb6c6a56a6
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-09 20:49:17 -0500

    Fix bug in login functionality

---
+++ scripts/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is for local testing. Its used by Air.
+
+rm one-click-aks-server
+
+export VERSION="$(date +%Y%m%d)"
+
+required_env_vars=("PROTECTED_LAB_SECRET" "VERSION")
+
+for var in "${required_env_vars[@]}"; do
+    if [[ -z "${!var}" ]]; then
+        echo "Required environment variable $var is missing"
+        exit 1
+    fi
+done
+
+
+go build -ldflags "-X 'main.version=$VERSION' -X 'one-click-aks-server/internal/entity.ProtectedLabSecret=$PROTECTED_LAB_SECRET'" ./cmd/one-click-aks-server
+
+redis-cli flushall
