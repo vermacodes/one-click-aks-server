f099195ed780dda58e5fc2d2d60b4a82f1f36490
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-01-22 12:51:05 -0500

    Add firewall_name output

--- tf/firewall_output.tf
+++ tf/firewall_output.tf
@@ -1,3 +1,7 @@
 output "firewall_private_ip" {
   value = length(var.firewalls) == 0 ? "" : azurerm_firewall.this[0].ip_configuration[0].private_ip_address
 }
+
+output "firewall_name" {
+  value = length(var.firewalls) == 0 ? "" : azurerm_firewall.this[0].name
+}
