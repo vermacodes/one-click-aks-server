4b1141f2f4b095eb3397c7a8eb950e107716d275
Adam Margherio <amargherio@microsoft.com>
2024-04-08 15:26:30 -0500

    fixed casing issues with node OS SKU and it's terraform attribute name

--- internal/entity/lab.go
+++ internal/entity/lab.go
@@ -14,7 +14,7 @@ type TfvarDefaultNodePoolType struct {
 	MaxCount                  int    `json:"maxCount"`
 	VmSize                    string `json:"vmSize"`
 	OnlyCriticalAddonsEnabled bool   `json:"onlyCriticalAddonsEnabled"`
-	NodeOsSku                 string `json:"nodeOsSku"`
+	OsSku                     string `json:"osSku"`
 }
 
 type TfvarServiceMeshType struct {
