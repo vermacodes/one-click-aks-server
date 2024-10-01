8e6e98423393f53a2ca536739fc0234f20a82c3e
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-10-01 17:30:33 -0400

    refactor: Refactor workspaces.sh script to improve readability and maintainability

--- scripts/workspaces.sh
+++ scripts/workspaces.sh
@@ -6,7 +6,27 @@
 OPTION=$1
 WORKSPACE=$2
 
-source $ROOT_DIR/scripts/helper.sh
+# We are not using function from helper.sh cause this function needs to be quiet. i.e. no output.
+function enableSharedKeyAccess() {
+  # Enable shared key access to storage account if not already enabled
+  sharedKeyAccess=$(az storage account show --name "$storage_account_name" -g "$resource_group_name" --query "allowSharedKeyAccess" --output tsv 2>/dev/null)
+  if [[ ${sharedKeyAccess} == "false" ]]; then
+    az storage account update --name "$storage_account_name" -g "$resource_group_name" --allow-shared-key-access true >/dev/null 2>&1
+  fi
+}
+
+# We are not using function from helper.sh cause this function needs to be quiet. i.e. no output.
+function init() {
+  # Initialize terraform only if not.
+  if [[ ! -f .terraform/terraform.tfstate ]] || [[ ! -f .terraform.lock.hcl ]]; then
+    terraform init \
+      -migrate-state \
+      -backend-config="resource_group_name=$resource_group_name" \
+      -backend-config="storage_account_name=$storage_account_name" \
+      -backend-config="container_name=$container_name" \
+      -backend-config="key=$tf_state_file_name" >/dev/null 2>&1
+  fi
+}
 
 function listWorkspaces() {
 
@@ -41,7 +61,8 @@ if [[ "$ARM_SUBSCRIPTION_ID" == "" ]]; then
   export ARM_SUBSCRIPTION_ID=$(az account show --output json --only-show-error | jq -r .id)
 fi
 
-tf_init
+enableSharedKeyAccess
+init
 
 if [[ "$OPTION" == "list" ]]; then
   listWorkspaces
