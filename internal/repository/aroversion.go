13d2abb79f3ca6c389e87f2814b8c4ca0367e40f
vermacodes <19263858+vermacodes@users.noreply.github.com>
2025-09-09 13:20:21 -0400

    fix: update author information in ARO extension installation script and refactor ARO versions caching logic

--- internal/repository/aroversion.go
+++ internal/repository/aroversion.go
@@ -33,7 +33,7 @@ func (a *aroVersionRepository) GetAROVersions(location string) (string, error) {
 	slog.Info("Getting ARO versions for location " + location)
 
 	// Check if the orchestrator versions are already cached in Redis
-	aroVersions, err := a.rdb.Get(context.Background(), "aroVersions").Result()
+	aroVersions, err := a.rdb.Get(context.Background(), "aroVersions-"+location).Result()
 	if err == nil {
 		return aroVersions, nil
 	}
@@ -61,6 +61,11 @@ func (a *aroVersionRepository) GetAROVersions(location string) (string, error) {
 	}
 	defer resp.Body.Close()
 
+	// Check if the response status code indicates success
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to get ARO versions: status code %d", resp.StatusCode)
+	}
+
 	// Read the response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
@@ -68,7 +73,7 @@ func (a *aroVersionRepository) GetAROVersions(location string) (string, error) {
 	}
 
 	// Set the response body in Redis
-	err = a.rdb.Set(context.Background(), "aroVersions", string(body), 0).Err()
+	err = a.rdb.Set(context.Background(), "aroVersions-"+location, string(body), 0).Err()
 	if err != nil {
 		slog.Error("failed to set aro versions in redis", err)
 	}
