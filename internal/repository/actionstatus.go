84b8a82153aa27c7b616c686774b6149bd8ddbf2
vermacodes <19263858+vermacodes@users.noreply.github.com>
2025-10-07 09:32:03 -0400

    refactor: Update Terraform and server notification methods to use user-specific keys in Redis

--- internal/repository/actionstatus.go
+++ internal/repository/actionstatus.go
@@ -60,20 +60,20 @@ func (a *actionStatusRepository) WaitForActionStatusChange(ctx context.Context) 
 
 func (a *actionStatusRepository) SetTerraformOperation(ctx context.Context, val string) error {
 	rdb := newActionStatusRedisClient()
-	if err := rdb.Set(ctx, "terraform-operation", val, 0).Err(); err != nil {
+	if err := rdb.Set(ctx, helper.GetUserIDFromContext(ctx)+"-terraform-operation", val, 0).Err(); err != nil {
 		return err
 	}
 
-	return rdb.Publish(ctx, "redis-terraform-operation-pubsub-channel", val).Err()
+	return rdb.Publish(ctx, helper.GetUserIDFromContext(ctx)+"-redis-terraform-operation-pubsub-channel", val).Err()
 }
 
 func (a *actionStatusRepository) GetTerraformOperation(ctx context.Context) (string, error) {
 	rdb := newActionStatusRedisClient()
-	return rdb.Get(ctx, "terraform-operation").Result()
+	return rdb.Get(ctx, helper.GetUserIDFromContext(ctx)+"-terraform-operation").Result()
 }
 
 func (a *actionStatusRepository) WaitForTerraformOperationChange(ctx context.Context) (string, error) {
-	rdb := newActionStatusRedisClient().Subscribe(ctx, "redis-terraform-operation-pubsub-channel")
+	rdb := newActionStatusRedisClient().Subscribe(ctx, helper.GetUserIDFromContext(ctx)+"-redis-terraform-operation-pubsub-channel")
 	defer rdb.Close()
 
 	for {
@@ -92,7 +92,7 @@ func (a *actionStatusRepository) SetServerNotification(ctx context.Context, val 
 		return err
 	}
 
-	return rdb.Publish(ctx, "redis-server-notification-pubsub-channel", val).Err()
+	return rdb.Publish(ctx, helper.GetUserIDFromContext(ctx)+"-redis-server-notification-pubsub-channel", val).Err()
 }
 
 func (a *actionStatusRepository) GetServerNotification(ctx context.Context) (string, error) {
@@ -101,7 +101,7 @@ func (a *actionStatusRepository) GetServerNotification(ctx context.Context) (str
 }
 
 func (a *actionStatusRepository) WaitForServerNotificationChange(ctx context.Context) (string, error) {
-	rdb := newActionStatusRedisClient().Subscribe(ctx, "redis-server-notification-pubsub-channel")
+	rdb := newActionStatusRedisClient().Subscribe(ctx, helper.GetUserIDFromContext(ctx)+"-redis-server-notification-pubsub-channel")
 	defer rdb.Close()
 
 	for {
